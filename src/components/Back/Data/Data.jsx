a59851f5d418b5a7c1592687554d51451d910491
zendayat <nmapzendaya@gmail.com>
2023-11-29 06:17:47 +0300

    Initialization

---
+++ src/components/Back/Data/Data.jsx
@@ -0,0 +1,42 @@
+const Data = {
+    productItems: [
+        {
+            id: "1",
+            name: "BeetsHeadphone",
+            price: 999,
+            image: "./images/pic1.jpeg"
+        },
+        {
+            id: "2",
+            name: "BeetsHeadphone",
+            price: 999,
+            image: "./images/pic2.jpeg"
+        },
+        {
+            id: "3",
+            name: "BeetsHeadphone",
+            price: 999,
+            image: "./images/pic3.jpeg"
+        },
+        {
+            id: "4",
+            name: "BeetsHeadphone",
+            price: 999,
+            image: "./images/pic4.jpeg"
+        },
+        {
+            id: "5",
+            name: "BeetsHeadphone",
+            price: 999,
+            image: "./images/pic5.jpeg"
+        },
+        {
+            id: "6",
+            name: "BeetsHeadphone",
+            price: 999,
+            image: "./images/pic6.jpeg"
+        },
+    ]
+}
+
+export default Data;
