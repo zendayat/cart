59daa92125837562af45f59199b862ea5cc28b1e
zendayat <nmapzendaya@gmail.com>
2023-12-09 07:29:40 +0300

    Cart Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
-import Data from './components/Back/Data/Data';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/navbar";
+import { Shop } from "./pages/shop/shop";
+import { Contact } from "./pages/contact";
+import { Cart } from "./pages/cart/cart";
+import { ShopContextProvider } from "./context/shop-context";
 
 function App() {
-  const {productItems} = Data;
   return (
     <div className="App">
-      GENOCIDE
+      <ShopContextProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Shop />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </Router>
+      </ShopContextProvider>
     </div>
   );
 }
